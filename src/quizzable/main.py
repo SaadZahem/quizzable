f6a57db2ee2ca4e882dcaaf749592ac261eb06e3
SaadZahem <saadzahem7@gmail.com>
2026-07-14 10:14:29 +0300

    feat: new design and a functional search bar

--- src/quizzable/main.py
+++ src/quizzable/main.py
@@ -1,36 +1,28 @@
 import operator as op
 import os
+import random
 import re
 
 from models import load_questionset
 from nicegui import ElementFilter, html, ui
 
-quizzes = [file for file in os.listdir("data") if file.endswith(".yml")]
+for tag in (f"h{n}" for n in range(2, 7)):
+    if not hasattr(html, tag):
+        setattr(html, tag, html._create_html_element(tag))
+
+quizzes = []
 
 
-def question_card(number, question, value=None, *, reveal=False):
-    with ui.card().classes("container") as card:
-        if reveal:
-            with ui.row().classes("w-full justify-end"):
-                with ui.column():
-                    question_text = "{}. {}".format(number, question.text)
-                    html.strong(question_text)
-                    for index, prefix, choice in zip(
-                        range(5), "abcde", question.choices
-                    ):
-                        choice_text = "{}) {}".format(prefix, choice)
-                        label = ui.label(choice_text).classes("py-1 px-2 rounded-lg")
+def _navigate(location):
+    def callback():
+        ui.navigate.to(location)
 
-                        if index == question.correct:
-                            label.classes("bg-green-300 text-[blue]")
-                        elif index == value:
-                            label.classes("bg-red-300 text-[blue]")
+    return callback
 
-                ui.space().classes("grow")
-                ui.separator().props("vertical")
-                grade_text = "%i/1" % (value == question.correct)
-                ui.label(grade_text).classes("my-auto text-end")
-        else:
+
+def question_card(number, question, value=None, *, review=False):
+    with ui.card().classes("self-stretch") as card:
+        if not review:
             html.strong(f"{number}. " + question.text)
             ui.radio(
                 {
@@ -41,21 +33,50 @@ def question_card(number, question, value=None, *, reveal=False):
                 },
                 value=value,
             )
-    return card.mark("question")
+            return card
+
+        # Question card for reviewing
+        with ui.row().classes("w-full justify-end"):
+            with ui.column():
+                question_text = "{}. {}".format(number, question.text)
+                html.strong(question_text)
+                for index, prefix, choice in zip(range(5), "abcde", question.choices):
+                    choice_text = "{}) {}".format(prefix, choice)
+                    label = ui.label(choice_text).classes("py-1 px-2 rounded-lg")
+
+                    if index == question.correct:
+                        label.classes("bg-green-300 text-[blue]")
+                    elif index == value:
+                        label.classes("bg-red-300 text-[blue]")
+
+            ui.space().classes("grow")
+            with ui.row().classes("self-stretch"):
+                ui.separator().props("vertical")
+                grade_text = "%i/1" % (value == question.correct)
+                ui.label(grade_text).classes("my-auto text-end")
+
+    return card
 
 
 @ui.page("/update")
-def update_page():
+def update_page(*, redirect=True):
     global quizzes
-    quizzes = [file for file in os.listdir("data") if file.endswith(".yml")]
-    ui.navigate.to("/")
+
+    quizzes = [
+        file.removesuffix(".yml")
+        for file in os.listdir("data")
+        if file.endswith(".yml")
+    ]
+
+    if redirect:
+        ui.navigate.to(home_page)
 
 
 @ui.page("/quiz/{file:str}")
 def quiz_page(file):
     assert file in quizzes
 
-    questions = load_questionset(f"data/{file}")
+    questions = load_questionset(f"data/{file}.yml")
 
     def submit():
         selection = ""
@@ -65,13 +86,15 @@ def quiz_page(file):
 
         ui.navigate.to(f"/quiz/{file}/{selection}")
 
-    with ui.column().classes("w-full items-center gap-2"):
+    ui.context.client.content.classes("bg-[wheat] min-h-[100vh]")
+
+    with ui.column().classes("h-full container mx-auto"):
         for number, question in questions.items():
             question_card(number, question)
 
         ui.separator()
-        ui.button("Submit", on_click=submit)
-        ui.space().classes("h-64")
+        ui.button("Submit", on_click=submit).classes("px-8 self-center")
+        ui.space().classes("h-32")
 
 
 @ui.page("/quiz/{file:str}/{selection:str}")
@@ -79,7 +102,7 @@ def result_page(file, selection):
     assert file in quizzes
     assert re.match("[=0-4]+", selection)
 
-    questions = load_questionset(f"data/{file}")
+    questions = load_questionset(f"data/{file}.yml")
     total = len(questions)
     score = sum(
         question.correct == int(choice)
@@ -93,36 +116,110 @@ def result_page(file, selection):
             for (number, question), char in zip(questions.items(), selection):
                 assert char in "=01234"
                 value = None if char == "=" else int(char)
-                question_card(number, question, value, reveal=True)
+                question_card(number, question, value, review=True)
 
-        action.set_visibility(False)
+        separator.set_visibility(True)
 
-    container = ui.column().classes("w-full items-center gap-2")
+    ui.context.client.content.classes("bg-[wheat] min-h-[100vh]")
+    with ui.column().classes("container mx-auto"):
+        container = ui.column().classes("self-stretch items-center gap-2")
+        separator = ui.separator().set_visibility(False)
+
+        with (
+            ui.card().classes("w-full md:w-md self-center"),
+            ui.row().classes("w-full"),
+        ):
+            ui.markdown(result).classes("grow text-center my-auto text-lg")
+            with ui.column().classes("items-stretch"):
+                ui.button(
+                    "Return",
+                    icon="home",
+                    on_click=_navigate("/home"),
+                )
+                ui.button(
+                    "Review",
+                    color="positive",
+                    icon="check",
+                    on_click=review,
+                ).bind_visibility_from(separator, "visible", op.not_)
+                ui.button(
+                    "Retry",
+                    color="negative",
+                    icon="repeat",
+                    on_click=_navigate(f"/quiz/{file}"),
+                ).bind_visibility_from(separator, "visible")
+
+
+def header_element():
+    with (
+        html.header().classes("w-full py-4 shadow-inner-lg") as header,
+        ui.row().classes("container mx-auto items-center"),
+    ):
+        with html.a().props('href="/"') as anchor, ui.element("h5"):
+            anchor.classes("hover:underline")
+            html.strong("Quizzable")
+
+        ui.space()
+        ui.link("update", "/update")
+
+    return header.classes("border-b-1 border-dashed border-slate")
+
+
+@ui.page("/home")
+def home_page():
+
+    @ui.refreshable
+    def quiz_list():
+        with ui.list().props("separator").classes("grow md:w-md"):
+            for file in quizzes:
+                for term in search.value:
+                    if term not in file:
+                        break
+                else:
+                    with ui.item():
+                        ui.link(file.replace(*"- ").title(), f"quiz/{file}")
+
+    ui.context.client.content.classes("p-0 gap-0 h-[100vh]")
+    header_element().classes("bg-[wheat]")
 
     with (
-        ui.card().classes("container md:w-md self-center"),
-        ui.row().classes("w-full justify-center"),
+        ui.element().classes("size-full py-8 bg-[wheat]"),
+        ui.row().classes("h-full container mx-auto"),
+        ui.card().classes("self-stretch grow justify-center items-center"),
     ):
-        ui.markdown(result).classes("grow text-center my-auto text-lg")
-        with ui.column().classes("items-stretch"):
-            ui.button("Home", on_click=lambda: ui.navigate.to("/"))
-            action = ui.button("Review").on("click", review)
-            ui.button(
-                "Retry", on_click=lambda: ui.navigate.to(f"/quiz/{file}")
-            ).bind_visibility_from(action, "visible", op.not_)
+        search = ui.input_chips("Search quizzes", on_change=quiz_list.refresh).classes(
+            "self-stretch"
+        )
+        quiz_list()
 
 
 @ui.page("/")
-def home_page():
-    with ui.row().classes("w-full"):
-        html.strong("Quizzable")
-        ui.link("update", "/update")
-        ui.space()
-        ui.label("Attempt your quizzes anytime")
+def landing_page():
+    ui.context.client.content.classes("p-0 gap-0 h-[100vh]")
+    header_element().classes("bg-[wheat]")
 
-    with ui.column():
-        for file in quizzes:
-            ui.link(file, f"quiz/{file}")
+    with (
+        ui.element().classes("size-full py-8 bg-[wheat]"),
+        ui.row().classes("h-full container mx-auto") as container,
+        ui.card().classes("self-stretch grow justify-center items-center"),
+    ):
+        hook_template = "{} your quizzes {}"
+        verb = random.choice(["Attempt", "Make", "Share"])
+        adverb = random.choice(["anytime", "anywhere", "anyhow"])
+        hook = hook_template.format(verb, adverb)
+
+        ui.label("Welcome to").classes("text-4xl")
+        html.strong("Quizzable").classes("text-6xl")
+        ui.label(hook).classes("italic text-2xl")
+        ui.button("Get started", on_click=_navigate("/home")).props(
+            'icon-right="arrow_forward" no-caps rounded'
+        ).classes("text-lg text-bold")
+
+    # I don't know what to make out of this yet
+    with container, ui.card().classes("hidden md:w-64 h-1/2 self-end"):
+        ui.skeleton(animation="wave").classes("w-full")
+        ui.card_section()
 
 
+update_page(redirect=False)
 ui.run()
