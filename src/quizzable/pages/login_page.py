9feed94ec2642cc9e36077b258d59172ec305936
SaadZahem <saadzahem7@gmail.com>
2026-07-18 18:38:41 +0300

    feat: animation in login page

--- src/quizzable/pages/login_page.py
+++ src/quizzable/pages/login_page.py
@@ -8,9 +8,10 @@ from ..widgets import header_element
 def card_template(action: str, primary: str, secondary: str, on_click: Callable):
     with (
         ui.card()
-        .props(f"tag=form method=post action={action}")
-        .classes("md:w-md md:px-8 justify-center items-stretch") as card
+        .classes("container max-w-md md:px-8 justify-center items-stretch aspect-4/3")
+        .props(f"tag=form method=post action={action}") as card
     ):
+        ui.space()
         ui.label(primary).classes("text-2xl text-bold text-center")
         (
             ui.input("Username")
@@ -22,6 +23,7 @@ def card_template(action: str, primary: str, secondary: str, on_click: Callable)
             .props("name=password")
             .on("keydown.enter", lambda: btn.run_method("click"))
         )
+        ui.space()
 
         with ui.card_actions().classes("text-xl"):
             btn = (
@@ -32,23 +34,38 @@ def card_template(action: str, primary: str, secondary: str, on_click: Callable)
             ui.space()
             ui.button(secondary, on_click=on_click).props("flat no-caps")
 
+        ui.space()
+
     return card
 
 
 @ui.page("/login")
-def login_page():
+def login_page(animate: bool = False):
+    # simple flip card animation using tailwind classes
+    if animate:
+        one = "z-1 rotate-y-0"
+        two = "rotate-y-180"
+        both = (
+            "w-full absolute-center transition-transform duration-1000 -translate-1/2"
+        )
+    else:
+        one = ""
+        two = "hidden"
+        both = "absolute-center"
 
     def switch():
         for card in (login_card, signup_card):
-            card.classes(toggle="hidden")
+            card.classes(toggle=f"{one} {two}")
 
     ui.query(".nicegui-content").classes("p-0 gap-0 h-[100vh]")
     header_element().classes("bg-[wheat]")
 
     with (
         ui.element().classes("size-full py-8 bg-[wheat]"),
-        ui.row().classes("h-full container mx-auto justify-center"),
+        ui.element().classes("h-full container mx-auto relative"),
     ):
         login_card = card_template("auth/token", "Log in", "Sign up", on_click=switch)
         signup_card = card_template("auth", "Sign up", "Log in", on_click=switch)
-        signup_card.classes("hidden")
+
+        login_card.classes(f"{one} {both}")
+        signup_card.classes(f"{two} {both}")
