4aa8f048abd808295f673d27bbeaf6259501762f
SaadZahem <saadzahem7@gmail.com>
2026-07-23 23:37:01 +0300

    feat: sometimes working quiz maker

--- src/quizzable/views/new_quiz_page.py
+++ src/quizzable/views/new_quiz_page.py
@@ -2,7 +2,7 @@ from nicegui import ui
 
 from ..models import User
 from ..utils import navigator, protected
-from ..widgets.question_card import EditableQuestionCard, QuestionCardContainer
+from ..widgets.question_card import QuestionCardContainer
 
 
 @protected
@@ -12,31 +12,31 @@ async def new_quiz_page(user: User):
     ):
         with ui.card().classes("items-center w-full max-w-md self-center") as main_card:
             ui.label("Create a new quiz").classes("text-2xl text-bold")
-            ui.input(prefix="Title:").props(
+            title_input = ui.input(prefix="Title:").props(
                 "autofocus dense outlined counter maxlength=255 autogrow"
             )
 
-        container = QuestionCardContainer[EditableQuestionCard]()
+        container = QuestionCardContainer(user, title_input)
         ui.separator()
         with ui.row().classes("items-center justify-center md:justify-evenly"):
             (
                 ui.button(color="secondary", icon="arrow_upward")
                 .on("click", navigator(main_card))
                 .props("outline")
-                .classes("px-2")
+                .classes("px-2 grow-1")
                 .tooltip("back to top")
             )
             (
                 ui.button(color="primary", icon="add")
                 .on("click", container.add_editable_question_card)
                 .props("outline no-caps")
-                .classes("px-2 grow")
+                .classes("px-2 grow-4")
                 .tooltip("add a question card")
             )
             (
                 ui.button(color="accent", icon="done")
-                .on("click", container.add_editable_question_card)
+                .on("click", container.create_quiz)
                 .props("outline no-caps")
-                .classes("px-2")
+                .classes("px-2 grow-1")
                 .tooltip("finish")
             )
--- src/quizzable/widgets/question_card.py
+++ src/quizzable/widgets/question_card.py
@@ -1,27 +1,67 @@
-from nicegui import ElementFilter, html, ui
+from nicegui import ElementFilter, background_tasks, html, ui
 from nicegui.elements.choice_element import ChoiceElement
 
-from ..models import MCQuestion
+from ..models import ChoiceEnum, MCQuestion, MCQuiz, User
 
 
-class QuestionCardContainer[T: ui.element](ui.column):
-    def __init__(self):
+class QuestionCardContainer(ui.column):
+    def __init__(self, user: User, title_input: ui.input):
         super().__init__(align_items="stretch")
         sortable = self.make_sortable(handle=".handle", group=self.__class__.__name__)
         sortable.disable()  # bugs
 
-        self.cards: list[T] = []
+        self.user = user
+        self.title_input = title_input
+        self.cards: list[EditableQuestionCard] = []
 
     def add_editable_question_card(self):
         with self:
             card = EditableQuestionCard()
             self.cards.append(card)
 
+    async def _create_quiz(self, values: list[dict[str, str]]):
+        new_quiz = await MCQuiz.create(
+            title=self.title_input.value.strip().title(),
+            maintainer=self.user,
+            tags="\n".join([]),
+        )
+
+        for questiondict in values:
+            await MCQuestion.create(
+                text=questiondict["q"],
+                a=questiondict["a"],
+                b=questiondict["b"],
+                c=questiondict["c"],
+                d=questiondict["d"],
+                e=questiondict.get("e"),
+                correct=ChoiceEnum(questiondict["k"]),
+                quiz=new_quiz,
+            )
+
+    def create_quiz(self):
+        values = []
+        try:
+            if not self.cards:
+                raise ValueError
+
+            for card in self.cards:
+                values.append(card.validate_values())
+
+        except ValueError:
+            ui.notify("The quiz cannot be empty", color="negative")
+        except AssertionError:
+            ui.notify("There must be a correct choice", color="negative")
+            ui.navigate.to(card)
+        else:
+            background_tasks.create(self._create_quiz(values))
+            ui.navigate.back()
+
 
 class EditableQuestionCard(ChoiceElement):
     def __init__(self, value: str | None = None):
         options = {prefix: "" for prefix in "abcde"}
         super().__init__(options=options, value=value or None)
+        self._choices = {}
         self._build()
 
     def _build(self):
@@ -29,8 +69,8 @@ class EditableQuestionCard(ChoiceElement):
             with (
                 ui.input()
                 .props("outlined dense autogrow")
-                .classes("self-stretch") as inp,
-                inp.add_slot("after"),
+                .classes("self-stretch") as self._text_input,
+                self._text_input.add_slot("after"),
                 ui.element(),
             ):
                 (
@@ -42,11 +82,10 @@ class EditableQuestionCard(ChoiceElement):
                 #     "handle cursor-grab active:cursor-grabbing"
                 # )
 
-            for index, prefix in enumerate("abcde", start=1):
+            for prefix in "abcde":
+                self._choices[prefix] = _inp = ui.input(prefix=f"{prefix})")
                 with (
-                    ui.input(prefix=f"{prefix})")
-                    .props("outlined dense autogrow")
-                    .add_slot("before"),
+                    _inp.props("outlined dense autogrow").add_slot("before"),
                     ui.button().props("flat round") as button,
                     ui.icon(
                         "radio_button_checked"
@@ -56,13 +95,34 @@ class EditableQuestionCard(ChoiceElement):
                 ):
                     button.on(
                         "click",
-                        lambda ico=icon, btn=button: (
-                            ElementFilter(kind=ui.icon, marker="radio")
-                            .within(instance=self)
-                            .props("name=radio_button_unchecked")
-                            and ico.props("name=radio_button_checked")
-                        ),
+                        lambda ico=icon, value=prefix: self._handle_change(ico, value),
                     )
+
+    def _handle_change(self, ico: ui.icon, value: str):
+        ElementFilter(kind=ui.icon, marker="radio").within(instance=self).props(
+            "name=radio_button_unchecked"
+        )
+        ico.props("name=radio_button_checked")
+        self.value = value
+
+    def validate_values(self) -> dict:
+        questiondict = dict(
+            q=self._text_input.value,
+            **{key: inp.value for key, inp in self._choices.items()},
+            k=self.value,
+        )
+
+        assert questiondict["q"]
+        assert questiondict["a"]
+        assert questiondict["b"]
+        assert questiondict["c"]
+        assert questiondict["d"]
+        assert questiondict["k"]
+
+        if not questiondict["e"]:
+            del questiondict["e"]
+
+        return questiondict
 
 
 def question_card(number, question: MCQuestion, value: str = "-", *, review=False):
